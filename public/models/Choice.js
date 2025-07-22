cfe7b174de5f1b7c1d2f90e971d7f292107dfb84
marwanelgharbawy <marwanelgharbawy2@gmail.com>
2025-07-22 20:25:49 +0300

    Delete prototyping models

--- public/models/Choice.js
+++
@@ -1,8 +0,0 @@
-class Choice {
-    constructor(name, price) {
-        this.name = name;   // Name of the choice (condiment)
-        this.price = price; // Price of the choice
-    }
-}
-
-export default Choice;
--- public/models/Options.js
+++
@@ -1,9 +0,0 @@
-class Options {
-    constructor(name, choices, isRadio) {
-        this.name = name;
-        this.choices = choices; // Choices for the option, such as the different condiments.
-        this.isRadio = isRadio; // Boolean
-    }
-}
-
-export default Options;
